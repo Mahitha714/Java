640bee27b6c47a2e6dd53a9505a911e0f167745d
Mahitha Budili <budilimahithareddy999@gmail.com>
2026-02-24 20:53:32 -0500

    Add Variables and Data Types example

---
+++ Src/VariableDemo.java
@@ -0,0 +1,15 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 24;
+        double height = 5.4;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "MahithaB";
+
+        System.out.println("Name: " +name);
+        System.out.println("age :"+ age);
+        System.out.println("Height:" + height);
+        System.out.println("garde: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
