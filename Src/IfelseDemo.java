e4737c581a363348b35d4a972b5fb18a1f9e0a87
Mahitha Budili <budilimahithareddy999@gmail.com>
2026-02-24 22:00:13 -0500

    Add IfElse Demo

---
+++ Src/IfelseDemo.java
@@ -0,0 +1,14 @@
+public class IfElseDemo {
+
+ public static void main(String[] args) {
+    int age = 23;
+
+    if (age > 18) {
+        System.out.println("You are an adult");
+
+    }
+    else {
+        System.out.println("you are a minor")
+    }
+ }
+}
